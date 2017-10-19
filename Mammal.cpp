f670e7177b6eec3abcc8b530aac5d128fce63aaa
chris morgan <chrisjamesmorgn@gmail.com>
2017-10-19 14:17:45 -0700

    New Mammal

--- Mammal.cpp
+++ Mammal.cpp
@@ -2,3 +2,32 @@
 // CISP 400 TUES/THURS
 // PROJECT 4
 // 2017-10-12
+
+#include "Mammal.h"
+
+Mammal::Mammal(): Animal()
+{
+
+}
+
+Mammal::Mammal(const char* name): Animal(name) {}
+
+Mammal::Mammal(const char* n, const GenderType& gt,
+		           double fc, double lf, const DietType& dt,
+							 const FeedType& ft, double bt, double tmpH, double tmpL)
+:Animal(n, gt, fc, lf, dt, ft)
+{
+	bodtTemp = bt;
+	tempHigh = tmpH;
+	tempLow = tmpL;
+}
+
+Mammal::Mammal(const Mammal& M): Animal(M)
+{
+
+}
+double Mammal::getBodyTemp()
+{
+  return bodyTemp;
+}
+
--- Mammal.h
+++ Mammal.h
@@ -2,3 +2,36 @@
 // CISP 400 TUES/THURS
 // PROJECT 4
 // 2017-10-12
+
+#ifndef MAMMAL_H_CJM
+#define MAMMAL_H_CJM
+
+#include <iostream>
+#include "animal.h"
+
+
+class Mammal: public Animal
+{
+  double bodyTemp;
+  double tempHigh;
+  double tempLow;
+
+Public:
+
+
+  Mammal();
+  Mammal(const char* );
+  Mammal(const char* n, const GenderType& gt,
+         double fc, double lf, const DietType& dt,
+         const FeedType& ft, double bt, double tmpH, double tmpL);
+  Mammal(const Mammal& );
+
+  ~Mammal();
+
+  double getBodyTemp();
+  void updateBodyTemp(double );
+  bool inRange();
+  
+};
+
+#endif
