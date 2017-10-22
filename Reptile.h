67b1f8d1ee0a5b7c9f5b6291f347f1797b3e0fd2
chris morgan <chrisjamesmorgn@gmail.com>
2017-10-21 20:38:15 -0700

    Rep

--- Reptile.h
+++ Reptile.h
@@ -2,3 +2,38 @@
 // CISP 400 TUES/THURS
 // PROJECT 4
 // 2017-10-12
+
+#ifndef REPTILE_H_CJM
+#define REPTILE_H_CJM
+
+#include <iostream>
+#include "animal.h"
+using namespace std;
+
+enum EnvironmentType {water, land};
+
+class Reptile: public Animal
+{
+  Environment type; 
+  
+
+public:
+
+	Reptile();
+	Reptile(const char* );
+	Reptile(const char* n, const GenderType& gt,
+			double fc, double lf, const DietType& dt,
+			const FeedType& ft, Environment T);
+	Reptile(const Reptile& );
+
+	~Reptile();
+
+	Reptile& operator= (const Reptile&);
+
+	Environment Reptile::getType() const;
+	void Reptile::updateType(Environment E);
+
+};
+#endif
+
+
