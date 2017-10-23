82268887a1805da24bd74cd9db3ff369555f1a36
chris morgan <chrisjamesmorgn@gmail.com>
2017-10-23 10:51:37 -0700

    N

--- Reptile.cpp
+++ Reptile.cpp
@@ -2,3 +2,48 @@
 // CISP 400 TUES/THURS
 // PROJECT 4
 // 2017-10-12
+
+#include "Reptile.h"
+
+Reptile::Reptile():Animal()
+{
+	type = land;
+}
+
+Reptile::Reptile(const char* name):Animal(name)
+{
+	type = land;
+}
+
+Reptile::Reptile(const char* n, const GenderType& gt,
+		double fc, double lf, const DietType& dt,
+		const FeedType& ft, const Environment& T)
+:Animal(n, gt, fc, lf, dt, ft, T)
+{
+  type = T;
+}
+
+Reptile::Reptile(const Reptile& R)
+:Animal(R)
+{
+  type = R.type;
+}
+
+Reptile::~Reptile() {}
+
+Reptile& Reptile::operator= (const Reptile& R)
+{
+	Animal::operator= (b);
+	type = R.type;
+	return *this;
+}
+
+Environment Reptile::getType() const
+{
+	return type;
+}
+
+void Reptile::updateType(const Environment& T)
+{
+  type = T;
+}
